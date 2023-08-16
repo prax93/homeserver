46a890eb081ff47bdf422132452087dd9a704919
u238471 <prashanth.sritharan@sbb.ch>
2023-08-16 15:44:04 +0200

    Modified Umbrella Script to prompt sudo password -K

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ECHO "Starting Installation Process (playbook.yml)"
-ansible-playbook -i ./hosts ./playbook.yml -C
+ansible-playbook -i ./hosts ./playbook.yml -K
 
 ECHO "Starting Containers (services.yml)"
-ansible-playbook -i ./hosts ./services.yml -C
+ansible-playbook -i ./hosts ./services.yml -K
