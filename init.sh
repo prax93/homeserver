a0ae0ee44535a3530804ed8727e8c919a1f79c15
Prashanth Sritharan <prax93@users.noreply.github.com>
2023-09-26 15:53:21 +0200

    Changed Folder Structure and modified init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ECHO "Starting Installation Process (playbook.yml)"
-ansible-playbook -i hosts.yml ./playbook.yml --check -K
+ansible-playbook -i ./compose/hosts.yml ../compose/playbook.yml --check -K
 
 ECHO "Starting Containers (services.yml)"
-ansible-playbook -i hosts.yml ./services.yml --check -K
+ansible-playbook -i ./compose/hosts.yml ../compose/services.yml --check -K
