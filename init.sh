076ea96bfd1ccf0413bf76c34ad75d0304ee7abe
prax93 <prashanthsritharan@Prashanths-MacBook-Pro-2.local>
2023-10-08 16:13:57 +0200

    Path fixed for playbooks

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 
 printf "Starting Installation Process (playbook.yml)\n\n"
-ansible-playbook -i ./compose/hosts.yml ../compose/playbook.yml --check -K
+ansible-playbook -i playbooks/hosts.yml playbooks/playbook.yml -C -K -b
 
 printf "$RED ATTENTION ! $NC Check the Media Location, by default the ansible playbook mounts them under /mnt/nfs. If you dont have a samba share please change the values from /mnt/nfs to the respective directory\n\n"
 sleep 10
@@ -14,4 +14,4 @@ nano ./compose/.env
 sleep 1
 
 printf  "Starting Containers (services.yml)"
-ansible-playbook -i ./compose/hosts.yml ../compose/services.yml --check -K
+ansible-playbook -i playbooks/hosts.yml playbooks/services.yml -C -K -b
