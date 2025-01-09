c4411beddfb10e1084cdd3659ebc9134bbe55c1d
prax93 <shana.sri@gmail.com>
2025-01-09 13:58:18 +0100

    Added Automatic deployment of custom transmission Gui

--- init.sh
+++ init.sh
@@ -10,3 +10,8 @@ ansible-playbook -i playbooks/hosts.yml playbooks/playbook.yml --ask-become-pass
 
 printf  "Starting Containers (services.yml)"
 ansible-playbook -i playbooks/hosts.yml playbooks/services.yml --ask-become-pass 
+
+git clone https://github.com/prax93/Transmission-WebGui.git &&
+cd ./Transmission-WebGui &&
+docker cp . transmission:/usr/share/transmission/public_html &&
+cd
